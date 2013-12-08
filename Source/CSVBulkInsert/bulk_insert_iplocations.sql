7bb35f87c1cef3552d0441986f203750632663f8
Gawaine O'Gilvie <gawaine.ogilvie@gmail.com>
2013-12-08 17:02:09 -0500

    bulk insert script

---
+++ Source/CSVBulkInsert/bulk_insert_iplocations.sql
@@ -0,0 +1,8 @@
+use ip;
+
+load data local infile 'C:/git/IISLoadDistribution/Source/CSVBulkInsert/GeoLiteCity-Location.csv' into table ip.ip_location
+fields terminated by ','
+enclosed by '"'
+lines terminated by '\n'
+(locId, country, region,city,postalCode,latitude,longitude,metroCode,areaCode) 
+
