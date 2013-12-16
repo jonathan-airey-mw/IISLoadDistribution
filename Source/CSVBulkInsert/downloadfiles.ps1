41f70aab3e7aad106dad6e4911961750ac586715
jonathan-airey-mw <jairey@merchantwarehouse.com>
2013-12-16 15:30:07 -0500

    updating powershell script to remove old files (have to remove hard coding later

--- Source/CSVBulkInsert/downloadfiles.ps1
+++ Source/CSVBulkInsert/downloadfiles.ps1
@@ -4,3 +4,15 @@ $webClient = New-Object System.Net.WebClient
 $url = "http://geolite.maxmind.com/download/geoip/database/GeoLiteCity_CSV/GeoLiteCity-latest.zip"
 $file = "$storageDir\GeoLiteCity-latest.zip"
 $webclient.DownloadFile($url, $file)
+
+#This part unzips the file.
+
+Remove-Item "C:\Users\gawaineo\Desktop\Perl scipts - traffic project\downloads\csv_files\*" -Force -Recurse -ErrorAction SilentlyContinue
+
+$shell = new-object -com shell.application
+$zip = $shell.nameSpace($file)
+
+foreach($item in $zip.items())
+{
+	$shell.namespace("C:\Users\gawaineo\Desktop\Perl scipts - traffic project\downloads\csv_files").copyhere($item)
+}
